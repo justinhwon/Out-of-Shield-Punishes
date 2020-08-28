499462a4ede72f91ee7f056642d999c3b964f53d
justinhwon <justinwon94555@gmail.com>
2020-08-28 04:05:05 -0700

    added toggling between oos options and shield safety

--- matchups/views.py
+++ matchups/views.py
@@ -50,8 +50,8 @@ class TermsView(generic.TemplateView):
 
 def MatchupSearchView(request):
     try:
-        shieldChar = request.GET['Shielding Character']
-        attackChar = request.GET['Attacking Character']
+        shieldChar = request.GET['Your Character']
+        attackChar = request.GET['Opponent Character']
         shieldCharData = Framedata.objects.filter(character=shieldChar)
         attackCharData = Framedata.objects.filter(character=attackChar)
     except:
@@ -224,9 +224,128 @@ def MatchupSearchView(request):
             punishList.append([punishOptions, punishableMoves])
         """
 
+        # Now get the opposite (shield safety of shieldChar's moves)
+        try:
+            shieldChar = request.GET['Opponent Character']
+            attackChar = request.GET['Your Character']
+            shieldCharData = Framedata.objects.filter(character=shieldChar)
+            attackCharData = Framedata.objects.filter(character=attackChar)
+        except:
+            # Error if character names not chosen correctly
+            raise Http404("Character name does not exist. Please choose a name from the dropdown.")
+        else:
+            # get all shield character moves and calculate their first frame OOS
+            # [name, frame oos, first frame not oos, frame data]
+            shieldCharMoves = []
+            for move in shieldCharData:
+                # moves that require dropping shield add 11 frames
+                if move.move in ("Jab 1", "Jab", "F-Tilt", "U-Tilt", "D-Tilt", "F-Smash", "D-Smash", "Jab 1 ", "Jab ", "F-Tilt ", "U-Tilt ", "D-Tilt ", "F-Smash ", "D-Smash ", "F-Smash (early)", "D-Smash (early)"):
+                    moveName = move.move
+                    moveStartupComplete = move.startup
+                    moveStartupFrame = int(re.findall(r'\d+', moveStartupComplete)[0])
+                    moveFrame = moveStartupFrame + 11
+                    shieldCharMoves.append([moveName, moveFrame, moveStartupFrame, moveStartupComplete])
+                # aerials add 3 frames
+                elif move.move in ("F-Air", "F-Air ", "N-Air", "N-Air ", "B-Air", "B-Air ", "D-Air", "D-Air ", "U-Air", "U-Air ", "Z-Air", "Z-Air ", "N-Air (either Dragon)", "N-Air (Ramram)", "N-Air (Megawatt)"):
+                    moveName = move.move
+                    moveStartupComplete = move.startup
+                    moveStartupFrame = int(re.findall(r'\d+', moveStartupComplete)[0])
+                    moveFrame = moveStartupFrame + 3
+                    shieldCharMoves.append([moveName, moveFrame, moveStartupFrame, moveStartupComplete])
+                # grabs add 4 frames
+                elif move.move in ("Grab", "Grab "):
+                    moveName = move.move
+                    moveStartupComplete = move.startup
+                    moveStartupFrame = int(re.findall(r'\d+', moveStartupComplete)[0])
+                    moveFrame = moveStartupFrame + 4
+                    shieldCharMoves.append([moveName, moveFrame, moveStartupFrame, moveStartupComplete])
+                # Dash attacks add 12 frames (11 shield drop + 1 to start dash)
+                elif move.move in ("Dash Attack", "Dash Attack "):
+                    moveName = move.move
+                    moveStartupComplete = move.startup
+                    moveStartupFrame = int(re.findall(r'\d+', moveStartupComplete)[0])
+                    moveFrame = moveStartupFrame + 12
+                    shieldCharMoves.append([moveName, moveFrame, moveStartupFrame, moveStartupComplete])
+                # Up-B and Upsmash are instantaneous
+                elif "(Up-B)" in move.move or move.move in ("U-Smash", "U-Smash "):
+                    # some up-b are not attacks (e.g. teleport), so just skip
+                    if not move.startup:
+                        continue
+                    moveName = move.move
+                    moveStartupComplete = move.startup
+                    moveStartupFrame = int(re.findall(r'\d+', moveStartupComplete)[0])
+                    moveFrame = moveStartupFrame
+                    shieldCharMoves.append([moveName, moveFrame, moveStartupFrame, moveStartupComplete])
+                #don't include other moves
+
+            # once list is complete, sort by frame oos (least to greatest)
+            shieldCharMoves.sort(key = lambda x: x[1])
+
+            # get all attack character moves and parse their frame advantage
+            # [name, frame advantage]
+            attackCharMoves = []
+            for move in attackCharData:
+                # don't include grab and make sure advantage number exists
+                if move.move not in ("Grab", "Grab ") and move.advantage:
+                    moveName = move.move
+                    # get the highest value advantage number
+                    moveAdvantage = max(list(map(int, re.findall(r'[+-]?\d+', move.advantage))))
+                    attackCharMoves.append([moveName, moveAdvantage, move.advantage])
+            
+            # once list is complete, sort by frame advantage (greatest to least)
+            attackCharMoves.sort(key = lambda x: x[1], reverse=True)
+
+            # now create lists of which attackChar moves are punishable by which shieldChar moves on shield
+            # keep building solution starting from fastest shieldChar moves
+            
+            safeList = []
+            #if at start there are unpunishable moves
+            if shieldCharMoves[0][1] + attackCharMoves[0][1] > 0:
+                punishOptions = []
+                punishableMoves = []
+                # pop from attackCharMoves until punishable
+                while shieldCharMoves[0][1] + attackCharMoves[0][1] > 0:
+                    punishableMoves.append(attackCharMoves.pop(0))
+                safeList.append([punishOptions, punishableMoves])
+            
+            # end loop when no moves remain
+            while shieldCharMoves or attackCharMoves:
+                punishOptions = []
+                punishableMoves = []
+
+                # if there remains a move that cannot punish anything, just end
+                if not attackCharMoves:
+                    break
+
+                # move CANNOT be punished OOS if frame disadvantage + startup > 0
+                # move CAN be punished OOS if frame disadvantage + startup <= 0
+
+                # pop from shieldCharMoves until attackCharMove cannot be punished
+                while shieldCharMoves and shieldCharMoves[0][1] + attackCharMoves[0][1] <= 0:
+                    punishOptions.append(shieldCharMoves.pop(0))
+
+                # if no more shieldCharMoves, only attackCharMoves left so put all 
+                # remaining attackCharMoves into punishable moves and finish up
+                if not shieldCharMoves:
+                    while attackCharMoves:
+                        punishableMoves.append(attackCharMoves.pop(0))
+                    safeList.append([punishOptions, punishableMoves])
+                    break
+                    
+                # current attackCharMove is not punishable by shieldCharMove
+                # pop from attackCharMove until it CAN be punished by shieldCharMove
+                while attackCharMoves and shieldCharMoves[0][1] + attackCharMoves[0][1] > 0:
+                    punishableMoves.append(attackCharMoves.pop(0))
+                
+                # punishOptions is now the limit of what moves can punish current punishableMoves
+                # punishableMoves is the limit of what moves can be punished by current punishOptions
+
+                safeList.append([punishOptions, punishableMoves])
+
         # Return all moves from each char
         return render(request, 'matchups/matchup.html', {
             'punishList': punishList,
-            'shieldChar': shieldChar,
-            'attackChar': attackChar,
+            'safeList': safeList,
+            'shieldChar': request.GET['Your Character'],
+            'attackChar': request.GET['Opponent Character'],
         })
