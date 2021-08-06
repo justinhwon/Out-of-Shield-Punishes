305869b179ff1084e706f7e0699443b4d1097eb6
justinhwon <justinwon94555@gmail.com>
2021-08-06 04:13:14 -0700

    Correct fix for Kazuya character page

--- matchups/views.py
+++ matchups/views.py
@@ -660,6 +660,9 @@ def CharacterView(request):
                 moveStartupComplete = move.startup
                 moveStartupFrame = int(re.findall(r'\d+', moveStartupComplete)[0])
                 moveFrame = moveStartupFrame + 3
+                # Kazuya jump squat is 7 frames instead of the normal 3
+                if shieldChar == "Kazuya":
+                    moveFrame = moveStartupFrame + 7
                 shieldCharMoves.append([moveName, moveFrame, moveStartupFrame, moveStartupComplete])
             # grabs add 4 frames
             elif move.move in ("Grab", "Grab "):
@@ -704,9 +707,6 @@ def CharacterView(request):
                     moveStartupComplete = move.startup
                     moveStartupFrame = int(re.findall(r'\d+', moveStartupComplete)[0])
                     moveFrame = moveStartupFrame + 3
-                    # Kazuya jump squat is 7 frames instead of the normal 3
-                    if shieldChar == "Kazuya":
-                        moveFrame = moveStartupFrame + 7
                     shieldCharMoves.append([moveName, moveFrame, moveStartupFrame, moveStartupComplete])
                 # grabs add 4 frames
                 elif move.move in ("Grab", "Grab "):
